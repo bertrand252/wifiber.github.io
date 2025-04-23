6263de5b905dd7260d3cabfac3477bd493b2d706
bertrand252 <bertrandleonard736@gmail.com>
2025-04-23 10:35:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,16 @@ let steps = [
     "Almost there...", "Wait for it...", "Password: 0910405289"
 ];
 let index = 0;
-let actualPassword = "0910405289";  // The correct Wi-Fi password
+let actualPassword = "0910405289";  
 
-// Reveal password step-by-step
+
 function revealPassword() {
     if (index < steps.length) {
         document.getElementById("password").innerText = steps[index];
         index++;
-        setTimeout(revealPassword, 1200);  // 1.2 seconds between each step
+        setTimeout(revealPassword, 1000);  
     } else {
-        // Show the QR code button after the password is revealed
         document.getElementById("qrButton").style.display = "block";
     }
 }
 
-// Function to display the QR code
-function showQRCode() {
-    document.getElementById("qrCode").innerHTML = '<img src="https://assets.onecompiler.app/42wsygy8d/42wsxgtwr/qr%20wifi.jpg" alt="qr wifi.jpg">';
-}
